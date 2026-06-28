6d64dfac6f1213dc344b250b9df642884cbd1d59
voxten <voxterman@gmail.com>
2026-06-29 00:08:50 +0200

    Secured project environment variables for production purposes

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -3,17 +3,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDtXMBaUCWrNZZcyJ_kfID3H1ppkdN_Vxk",
-    authDomain: "series-tracker-e8419.firebaseapp.com",
-    projectId: "series-tracker-e8419",
-    storageBucket: "series-tracker-e8419.firebasestorage.app",
-    messagingSenderId: "612189787516",
-    appId: "1:612189787516:web:03db2fe323a02ecb0bc0f9"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { db, auth, provider  };
+export { db, auth, provider };
